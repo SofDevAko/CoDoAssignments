f18796d095e443bce3a4143c73b10d141b2758e1
Akin <sofdevako@gmail.com>
2018-04-12 15:28:55 -0500

    Added Angular DOM part assignment RESTFUL tasks continued

---
+++ MEAN/Angular/DOMmanipulation/RestfulTasksContinued/HelloAngular/src/app/app.component.ts
@@ -0,0 +1,30 @@
+
+import { HttpService } from './http.service';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  tasks = [];
+  title = 'HelloAngular';
+  constructor(private _httpService: HttpService){}
+  
+  ngOnInit(){
+    this.getTasksFromService();
+  }
+  getTasksFromService(){
+    let observable = this._httpService.getTasks();
+    observable.subscribe(data => {
+      console.log("Got our tasks!", data)
+      // In this example, the array of tasks is assigned to the key 'tasks' in the data object.
+      // This may be different for you, depending on how you set up your Task API.
+      this.tasks = data['data'];
+      console.log(this.tasks)
+    });
+  }
+
+}
